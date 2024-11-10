326e14bfffe7a1b57b11de9f84859a75bb5c126c
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2024-11-10 01:41:28 +0100

    Pin dotnet to version 6

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ apt update
 apt install git wget libicu-dev
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh
+./dotnet-install.sh --version 6.0.427
 export PATH=$PATH:$HOME/.dotnet
 export DOTNET_ROOT=$HOME/.dotnet
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
